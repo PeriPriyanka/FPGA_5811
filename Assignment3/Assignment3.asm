9b5f9effb305a085362a9b87d9c9d9598afde375
PeriPriyanka <89184904+PeriPriyanka@users.noreply.github.com>
2022-03-03 13:28:08 +0530

    Add files via upload

---
+++ Assignment3/Assignment3.asm
@@ -0,0 +1,46 @@
+.include "/Users/Priyanka/assembly/m328Pdef/m328Pdef.inc"
+ldi r17, 0b11110011 ; identifying input pins 2,3
+	out DDRD,r17		; declaring pins as input
+	ldi r17, 0b11111111 ;
+	out PORTD,r17		; activating internal pullup for pins 10,11,12,13  
+	in r17,PIND
+
+ldi r16, 0b00100000 ;identifying output pins 13
+out DDRD,r16		;declaring pins as output
+lsr r17
+lsr r17
+mov r16, r17
+ldi r20,0b00000001
+and r17,r20 ;V
+mov r18,r16
+lsr r18
+and r18, r20 ;U
+mov r0,r20
+mov r19,r17
+and r19,r18 ; UV
+mov r21,r17
+eor r21,r20 
+and r21,r18 ;UV'
+mov r22,r17
+eor r22, r20
+mov r23, r18
+eor r23,r20
+and r22, r23 ;U'V'
+or r19,r21
+or r19,r22 ; U'V'+UV'+UV
+mov r21, r17
+eor r21, r20
+or r21, r18 ;U+V'
+sub r21,r19 
+breq loop
+loop:
+ldi r16, 0b00100000
+out PORTB,r16
+;writing output to pins 2,3,4,5
+
+Start:
+rjmp Start
+
+
+
+
